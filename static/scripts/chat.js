1db8fea793006353cf5c0555f992ba845e5beb70
eurosandcents <2kul4yhu@gmail.com>
2026-09-16 18:43:52 +0100

    Fixed db updating during chats, also styling

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -39,6 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 }, false);
 
+function save_message() {
+    socket.emit("save_message", {
+        message: txt.value, sender_id:`${userId}`, ids:[userId, ...recipientIds]
+    });
+    console.log(`Saved: ${txt.value}`);
+}
+
 function send_message(e) {
     if (!txt.value.trim()) return;
 
@@ -48,10 +55,9 @@ function send_message(e) {
 
     if (save_timer) clearTimeout(save_timer);
     save_timer = setTimeout(() => {
-        socket.emit("save_message", {
-            message: txt.value, sender_id:`${userId}`, ids:[userId, ...recipientIds]
-        });
-        console.log(`Saved: ${txt.value}`);
+        if (txt.value.trim() !== "") {
+            save_message();
+        }
     }, 300);
 }
 
@@ -59,6 +65,7 @@ window.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         send_message();
         e.preventDefault();
+        save_message();
         txt.value = "";
     }
 });
